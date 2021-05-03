ba6f71e1a0317a785ae7141c3bc6305f9dacc56e
Renan Lopes <renanlopes.ads@gmail.com>
2021-05-03 20:37:34 -0300

    Projetos Criados

---
+++ backend/src/main/java/com/renanlopes/rlvendas/RlvendasApplication.java
@@ -0,0 +1,13 @@
+package com.renanlopes.rlvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/renanlopes/rlvendas/RlvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanlopes.rlvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

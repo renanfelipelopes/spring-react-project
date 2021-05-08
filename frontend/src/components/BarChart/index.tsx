defb362c769eae0182308e854eb97bc7a22495d8
Renan Lopes <renanlopes.ads@gmail.com>
2021-05-08 17:51:55 -0300

    Deploy Netlify env var

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -8,45 +8,44 @@ import { BASE_URL } from "utils/requests";
 type SeriesData = {
   name: string;
   data: number[];
-}
+};
 
 type ChartData = {
   labels: {
     categories: string[];
   };
   series: SeriesData[];
-}
+};
 
 const BarChart = () => {
-
   const [chartData, setChartData] = useState<ChartData>({
     labels: {
-      categories: []
+      categories: [],
     },
     series: [
       {
         name: "",
-        data: []
-      }
-    ]
+        data: [],
+      },
+    ],
   });
 
   useEffect(() => {
     axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
       const data = response.data as SaleSuccess[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1));
 
       setChartData({
         labels: {
-          categories: myLabels
+          categories: myLabels,
         },
         series: [
           {
             name: "% Success",
-            data: mySeries
-          }
-        ]
+            data: mySeries,
+          },
+        ],
       });
     });
   }, []);
